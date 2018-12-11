46159c851ed6c8d35bd8f4036e1607d9137fe242
steve3p0 <stevebpdx@gmail.com>
2018-12-10 22:48:42 -0800

    i hate c

--- newtonTests.h
+++ newtonTests.h
@@ -12,19 +12,25 @@
 #ifndef NEWTON_NEWTONTESTS_H
 #define NEWTON_NEWTONTESTS_H
 
+#define _GNU_SOURCE
+
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <stdbool.h>
 #include "newton.h"
 
 typedef struct test
 {
+    int index;
     int count;
     int pass;
 
-    char testname [100][1000];
-    char results  [100][1000];
-    char messages [100][1000];
-    char failed   [100][1000];
+    //char **testname;
+    char testname [10][100];
+    char results  [10][100];
+    char messages [10][10];
+    char failed   [10][10];
 
     char stdinout[1000];
     int passfail[100];
@@ -55,17 +61,6 @@ bool CompareArrays(int n, double *a, double *b)
     return true;
 }
 
-char* concat(const char *s1, const char *s2)
-{
-    char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
-    // in real code you would check for errors in malloc here
-    strcpy(result, s1);
-    strcat(result, s2);
-
-    return result;
-}
-
-
 void print_array(int n, double *array, char *name)
 {
     printf("\n");
@@ -77,37 +72,122 @@ void print_array(int n, double *array, char *name)
     printf("\n");
 }
 
+//char *concat(const char *s1, const char *s2)
+char *concat(char *s1, char *s2)
+{
+    if (s1 == NULL)
+        return s2;
+    else if (s2 == NULL)
+        return s1;
+
+    char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
+    // in real code you would check for errors in malloc here
+    strcpy(result, s1);
+    strcat(result, s2);
+
+    return result;
+}
+
+char *concat1(char *s1, char *s2)
+{
+
+    int strlen_s1 = 0;
+    int strlen_s2 = 0;
+
+    if (s1 != NULL)
+        strlen_s1 = strlen(s1);
+
+    if (s2 != NULL)
+        strlen_s2 = strlen(s2);
+
+    char *result = malloc(strlen_s1 + strlen_s2 + 1); // +1 for the null-terminator
+    // in real code you would check for errors in malloc here
+    strcpy(result, s1);
+    strcat(result, s2);
+
+    return result;
+}
+
+
 ////////////////////////////////////////////////////
 
-int test_dummy(char *test_name, char *output)
+int test_dummy1(char *testname, char *output)
 {
-    printf("PASS - %s\n", test_name);
+    //printf("PASS - %s\n", test_name);
     //printf("FAIL - %s\n", test_name);
+
+
+//    char *s = concat("PASS - ", test_name);
+//    //s = concat(s, "\n");
+//    output = concat(output, s);
+
+    //char *new_str;
+    //asprintf(&new_str,"%s%s",str1,str2);
+
+//    asprintf(&new_str,"%s%s",str1,str2);
+
     return 1;
 }
+
+//////////////////////////////////////////////////////
+
+
+void test_dummy(char *testname, Test *t)
+{
+
+    strcpy(t->testname[t->index], testname);
+
+    // PASS
+    //*t->results[t->index] = "PASS";
+    strcpy(t->results[t->index], "PASS");
+    t->pass++;
+    //strcpy(t->results[t->index], "PASS");
+
+    // FAIL
+    //strcpy(t->results[t->index], "FAIL");
+
+    t->index++;
+    t->count++;
+}
+
 
 // TODO: Finish rest of unit tests
 int test_runner()
 {
     printf("\nRunning Tests...\n\n");
 
-    int count = 0;
-    int pass = 0;
-    char test_output[1000];
-    //char *output;
+    //char test_output[1] = " ";
+    //char *test_output = concat("Fuck ", "Off");
+    char *test_output = "fuck strings in C";
 
-    //Test t
+    Test *t = malloc(sizeof(Test));
+    t->index = 0;
+    t->count = 0;
+    t->pass = 0;
 
-    count++;
-    pass += test_dummy("test_dummy1", test_output);
+    test_dummy("test_dummy1", t);
+    test_dummy("test_dummy2", t);
+    test_dummy("test_dummy3", t);
 
-    count++;
-    pass += test_dummy("test_dummy2", test_output);
+    for (int i = 0; i < t->count; i++)
+    {
+        //printf("%d: (%s) - (%s)\n", i, t->testname[i], t->results[i]);
+        printf("\t%s - %s\n", t->results[i], t->testname[i]);
+    }
 
-    count++;
-    pass += test_dummy("test_dummy3", test_output);
+//    count++;
+//    pass += test_dummy("test_dummy1", test_output);
+//
+//    count++;
+//    pass += test_dummy("test_dummy2", test_output);
+//
+//    count++;
+//    pass += test_dummy("test_dummy3", test_output);
+//
+//    printf("%s", test_output);
+//    printf("\nTest Runner: %d out of %d tests PASSED\n", pass, count);
 
-    printf("\nTest Runner: %d out of %d tests PASSED\n", pass, count);
+    //free(test_output); // deallocate the string
 }
 
 
